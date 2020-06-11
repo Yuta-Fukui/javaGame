12f94fdcec2ea2c45089d5d7c545f38db9bed7c6
多岐正行 <tagimasayuki@MacBook-Pro.local>
2020-06-11 16:50:59 +0900

    insert

--- api/src/main/resources/db/migration/V1__create_tables.sql
+++ api/src/main/resources/db/migration/V1__create_tables.sql
@@ -1,6 +1,21 @@
 -- create sqls
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `Question` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `questionStatement` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `answer1` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `answer2` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `answer3` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `answer4` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `questionStatement` (`questionStatement`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `Correct_Answer` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `questionId` bigint NOT NULL,
+  `correctAnswer` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `explanation` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `questionId` (`questionId`),
+  CONSTRAINT `Correct_Answer_ibfk_1` FOREIGN KEY (`questionId`) REFERENCES `Question` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
