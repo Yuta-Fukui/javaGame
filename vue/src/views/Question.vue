96184fff47914cd5548e4d23fde0d5351d886969
福飯　雄太 <yuta@fukuhanyuutanoMacBook-Air.local>
2020-06-30 15:14:55 +0900

    テスト

--- vue/src/views/Question.vue
+++ vue/src/views/Question.vue
@@ -5,22 +5,22 @@
       {{ selectQuestion }}
     </p>
     <router-link to="/answer">
-      <button @click="answer()">
+      <button @click="answer('answer1')">
         {{ selectAnswer1 }}
       </button>
     </router-link>
     <router-link to="/answer">
-      <button @click="answer()">
+      <button @click="answer('answer2')">
         {{ selectAnswer2 }}
       </button>
     </router-link>
     <router-link to="/answer">
-      <button @click="answer()">
+      <button @click="answer('answer3')">
         {{ selectAnswer3 }}
       </button>
     </router-link>
     <router-link to="/answer">
-      <button @click="answer()">
+      <button @click="answer('answer4')">
         {{ selectAnswer4 }}
       </button>
     </router-link>
@@ -39,29 +39,32 @@ export default {
       return this.$store.state.question.contents.questionStatement
     },
     selectAnswer1 () {
-      return this.$store.state.question.contents.answer1
+      return this.$store.state.question.contents.answer1 == 'answer1';
     },
     selectAnswer2 () {
-      return this.$store.state.question.contents.answer2
+      return this.$store.state.question.contents.answer2 === 'answer2'
     },
     selectAnswer3 () {
-      return this.$store.state.question.contents.answer3
+      return this.$store.state.question.contents.answer3 === 'answer3'
     },
     selectAnswer4 () {
-      return this.$store.state.question.contents.answer4
+      return this.$store.state.question.contents.answer4 === 'answer4'
     },
     loaded () {
       return this.$store.state.question.loaded
     }
   },
   methods: {
-    answer () {
-    // const id = this.$store.state.answer.id
+    answer (answer) {
+      const selectedAnswer = answer
       const questionId = this.$store.state.question.contents.id
+      console.log(selectedAnswer)
       this.$store.dispatch('getAnswer', questionId)
+      this.$store.commit('getAnswer', selectedAnswer)
     },
     count () {
-      return this.$store.state.question.count
+      const count = this.$store.state.question.count
+      return count
     }
   }
 }
